87a46ae83d47a8944ada37afe220f121c76a7926
JustDava <dava.manukov@yandex.ru>
2021-04-14 21:41:09 +0300

    Last fix

--- TIPnS-Lab_2/Program.cs
+++ TIPnS-Lab_2/Program.cs
@@ -1,36 +1,56 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TIPnS_Lab_2
 {
-    class Letters
-    {
-        public char c;
-        public int[] bincode = new int[4];
-        public double prob;
-    }
+
     class Program
     {
+        public static int[] generate_binarycode()
+        {
+            int bite;
+            int[] biteCode = new int[4];
+            Random random = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                bite = random.Next(0, 2);
+                biteCode[i] = bite;
+            }
+            return biteCode;
+        }
+
+        public static int GetIndex(int[,] ar1, int[] ar2)
+        { 
+            for (int i = 0; i < ar1.GetLength(0); i++)
+            {
+                int[] ar = new int[4];
+                for (int j = 0; j < ar1.GetLength(1); j++)
+                {
+                    ar[j] = ar1[i, j];
+                    if (ar.SequenceEqual(ar2))
+                    {
+                        return i;
+                    }
+                    
+                }
+            }
+            return 10;                 
+        }
+
         static void Main(string[] args)
         {
-            List<Letters> letters = new List<Letters>();
+            Random random = new Random();
 
             double[] p = { 0.58, 0.42, 0.65, 0.35 };
-            double[,] kmi = new double[10, 12];
-            double[,] kmo = new double[10, 12];
+
+            double[,] apost_kmi = new double[10, 12];
+            double[,] apost_kmo = new double[10, 12];
             double[] PVj = new double[11];
 
             int height = 10;
             int width = 12;
-
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width-1; j++)
-                {
-                    kmi[i, j] = 1;
-                }
-            }
 
             char[] c = { 'e', 'a', 'd', 'i', 'g', 'c', 'f', 'b', 'h', 'j' };
             int[,] bincodes = {
@@ -42,102 +62,67 @@ namespace TIPnS_Lab_2
                 {0, 1, 0, 1 },
                 {0, 1, 1, 0 },
                 {0, 1, 1, 1 },
-                {1, 0, 0, 0 },
+                {1, 1, 0, 0 },
                 {1, 0, 0, 1 }
             };
-            double[] probs = { 0.24415, 0.22843, 0.08849, 0.21624, 0.02974, 0.12506, 0.02401, 0.02572, 0.01304, 0.00512 };
+            double[] probs = { 0.24415, 0.22843, 0.08849, 0.21624, 0.02974, 0.12506, 0.02401, 0.02572, 0.01304, 0.00512 };    
+
+            int[] biteCode = new int[4];
+
+            Console.WriteLine("Апостериорная канальная матрица источника:\n");
 
             for (int i = 0; i < 10; i++)
             {
-                Letters letter = new Letters();
-                letter.c = c[i];
-                letter.prob = probs[i];
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < 500; j++)
                 {
-                    letter.bincode[j] = bincodes[i, j];
+                    biteCode = generate_binarycode();
+                    apost_kmi[i, GetIndex(bincodes, biteCode)] += (double)1 / 500;
                 }
-                letters.Add(letter);
-            }
-            double sum = 0;
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    if (j != 10)
-                    {
-                        if (j != 11)
-                        {
-                            for (int k = 0; k < 4; k++)
-                            {
-                                if ((letters[i].bincode[k] == 0) && (letters[j].bincode[k] == 0))
-                                {
-                                    kmi[i, j] *= p[0];
-                                }
-                                if ((letters[i].bincode[k] == 1) && (letters[j].bincode[k] == 1))
-                                {
-                                    kmi[i, j] *= p[2];
-                                }
-                                if ((letters[i].bincode[k] == 0) && (letters[j].bincode[k] == 1))
-                                {
-                                    kmi[i, j] *= p[1];
-                                }
-                                if ((letters[i].bincode[k] == 1) && (letters[j].bincode[k] == 0))
-                                {
-                                    kmi[i, j] *= p[3];
-                                }
-                            }
-                            sum += kmi[i, j];
-                            kmi[i, 11] += kmi[i, j] * Math.Log2(kmi[i, j]);
-                        }
-                    }
-                    else
-                    {
-                        kmi[i, j] = 1 - sum;
-                        sum = 0;
-                        kmi[i, 11] += kmi[i, j] * Math.Log2(kmi[i, j]);
-                    }
-                }
-            }
-            Console.WriteLine("КМИ:");
-            for (int i = 0; i < height; i++)
-            {
-                for (int j = 0; j < width; j++)
-                {
-                    kmi[i, j] = Math.Round(kmi[i, j], 5);
-                    kmi[i, j] = Math.Abs(kmi[i, j]);
-                    Console.Write(kmi[i,j] + "\t");
-                }
-                Console.WriteLine();
             }
 
-            Console.WriteLine("\n\nКМО:");
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
                     if (j != 11)
                     {
-                        kmo[i, j] = kmi[i, j] * letters[i].prob;
-                        kmo[i, 11] += kmo[i, j] * Math.Log2(kmo[i, j]);                                    
+                        apost_kmi[i, 11] += apost_kmi[i, j] * Math.Log2(apost_kmi[i, j]);
                     }
-                    kmo[i, j] = Math.Round(kmo[i, j], 5);
-                    kmo[i, j] = Math.Abs(kmo[i, j]);
-                    Console.Write(kmo[i, j] + "\t");
+                    apost_kmi[i, j] = Math.Round(apost_kmi[i, j], 5);
+                    apost_kmi[i, j] = Math.Abs(apost_kmi[i, j]);
+                    Console.Write(apost_kmi[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("\n\nАпостериорная канальная матрица объединения:\n");
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (j != 11)
+                    {
+                        apost_kmo[i, j] = apost_kmi[i, j] * probs[i];
+                        apost_kmo[i, 11] += apost_kmo[i, j] * Math.Log2(apost_kmo[i, j]);
+                    }
+                    apost_kmo[i, j] = Math.Round(apost_kmo[i, j], 5);
+                    apost_kmo[i, j] = Math.Abs(apost_kmo[i, j]);
+                    Console.Write(apost_kmo[i, j] + "\t");
                 }
                 Console.WriteLine();
             }
 
             Console.WriteLine("\n\nP(Vj):");
-            for (int i = 0; i < width-1; i++)
+            for (int i = 0; i < width - 1; i++)
             {
                 for (int j = 0; j < height; j++)
                 {
-                    PVj[i] += kmo[j, i];
+                    PVj[i] += apost_kmo[j, i];
                     PVj[i] = Math.Round(PVj[i], 5);
                 }
                 Console.Write(PVj[i] + "\t");
             }
-
 
             double entropySrc = 0;
             for (int i = 0; i < probs.Length; i++)
@@ -160,14 +145,14 @@ namespace TIPnS_Lab_2
             double entropyNoise = 0;
             for (int i = 0; i < probs.Length; i++)
             {
-                entropyNoise += probs[i] * kmi[i,11];
+                entropyNoise += probs[i] * apost_kmi[i, 11];
             }
             entropyNoise = Math.Round(entropyNoise, 5);
             entropyNoise = Math.Abs(entropyNoise);
             Console.WriteLine("Энтропия шума: H(V|U) = " + entropyNoise);
 
             double entropyLeak = entropySrc - entropyRcv + entropyNoise;
-            Console.WriteLine("Энтропия утечки: H(V|U) = " + entropyLeak);
+            Console.WriteLine("Энтропия утечки: H(V|U) = " + Math.Round(entropyLeak, 5));
 
             double countofUsefulInfo = entropyRcv - entropyNoise;
             countofUsefulInfo = Math.Round(countofUsefulInfo, 5);
